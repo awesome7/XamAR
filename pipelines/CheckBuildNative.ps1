e4979e84d74818058ae6c9929f5f67850e548a5c
Bojan Misic <misic.bojan@gmail.com>
2021-05-24 14:43:48 +0200

    Troubleshooting PS scripts

--- pipelines/CheckBuildNative.ps1
+++ pipelines/CheckBuildNative.ps1
@@ -1,11 +1,9 @@
-param([String]$projects='') 
-
 [bool] $global:buildNative = $false;
 
-$projects.Split(';') | ForEach-Object {
+$env:BUILDQUEUE.Split(';') | ForEach-Object {
     if(-Not $_.Contains("Forms")) {
         $global:buildNative = $true;
     }
 }
 
-Write-Host "##vso[task.setvariable variable=buildNative;isOutput=true]"$global:buildNative""
+Write-Host "##vso[task.setvariable variable=buildNative;isOutput=true]$global:buildNative"
